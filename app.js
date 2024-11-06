1179e806c319e5c0fe5eed09d3fbae079a84e113
thekester <theophile.avenel@gmail.com>
2024-11-06 09:55:44 +0100

    Add the product list

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// app.js
 const express = require('express');
 const path = require('path');
 
@@ -10,18 +11,27 @@ app.set('views', path.join(__dirname, 'views'));
 // Middleware pour servir les fichiers statiques (comme jQuery)
 app.use('/code', express.static(path.join(__dirname, 'public', 'code')));
 
-// Route principale
+// Données des produits (pour l'instant définies en dur)
+const produits = [
+  { nom: 'Smartphone Alpha', annee: 2023, prix: 699.99, stock: 25, numeroSerie: 'SN123456' },
+  { nom: 'Ordinateur Portable Beta', annee: 2024, prix: 1299.99, stock: 15, numeroSerie: 'SN234567' },
+  { nom: 'Tablette Gamma', annee: 2022, prix: 399.99, stock: 40, numeroSerie: 'SN345678' },
+  { nom: 'Montre Connectée Delta', annee: 2023, prix: 199.99, stock: 60, numeroSerie: 'SN456789' },
+  { nom: 'Casque Audio Epsilon', annee: 2024, prix: 149.99, stock: 30, numeroSerie: 'SN567890' }
+];
+
+// Route principale pour afficher la liste des produits
 app.get('/', (req, res) => {
-  res.render('temp_exemple', { 
-    title: 'Bonjour', 
-    noms: ['Pauline', 'Jean', 'Jacques'] 
+  res.render('index', { 
+    title: 'Liste des Produits', 
+    produits: produits 
   });
 });
 
-// Route pour gérer la requête AJAX
-app.get('/bonjour/:name', (req, res) => {
+// Route pour gérer une requête AJAX
+app.get('/bonjour/:nom', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ message: 'Bonjour ' + req.params.name + '!' }));
+  res.send(JSON.stringify({ message: 'Bonjour ' + req.params.nom + '!' }));
 });
 
 // Démarrage du serveur
